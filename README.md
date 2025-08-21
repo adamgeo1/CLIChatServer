50c73ead8a2f94d4a426f8f20bdfb9d440bf0829
adamgeo1 <adamgeo66@gmail.com>
2025-08-21 02:45:30 -0400

    implemented rudimentary json input formatting, redid CMakeLists.txt and updated README.md

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ I am making this project for the purpose of learning C++, networking, security, 
 * https://medium.com/@naseefcse/ip-tcp-programming-for-beginners-using-c-5bafb3788001
 * https://en.cppreference.com/index.html
 * https://chatgpt.com (for guidelines, milestones, bug finding, etc.)
+* Lohmann, N. (2025). JSON for Modern C++ (Version 3.12.0) [Computer software]. https://github.com/nlohmann
--- client/main.cpp
+++ client/main.cpp
@@ -1,15 +1,90 @@
 #include <iostream>
 #include <string>
-#include <memory>
 #include <cstring>
+#include <format>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <json.hpp>
+#include <sstream>
+#include <vector>
+#include <fstream>
 
 constexpr int PORT = 8080;
 constexpr int BUFFER_SIZE = 1024;
+
+using namespace std;
+using namespace nlohmann;
+
+vector<string> split(const string& str, const char delim) {
+    istringstream iss(str);
+    string temp;
+    vector<string> tokens;
+
+    while (getline(iss, temp, delim)) {
+        if (!temp.empty()) {
+            tokens.push_back(temp);
+        }
+    }
+    return tokens;
+}
+
+json msg_to_json(const int req_id, const string& msg) {
+    json j;
+    j["req_id"] = req_id;
+    vector<string> split_msg = split(msg, ' ');
+    string cmd = split_msg[0].substr(1);
+    j["command"] = cmd;
+    if (cmd == "signup" or cmd == "login") {
+        j["args"] = {
+            {"username", split_msg[1]},
+            {"password", split_msg[2]}
+        };
+    }
+    else if (cmd == "join") {
+        j["args"] = split_msg[1];
+    }
+    else if (cmd == "msg") {
+        ostringstream oss;
+        for (size_t i = 2; i < split_msg.size(); ++i) {
+            if (i > 2) oss << ' ';
+            oss << split_msg[i];
+        }
+        j["args"] = {
+            {"room", split_msg[1]},
+            {"text", oss.str()}
+        };
+    }
+    else if (cmd == "dm") {
+        ostringstream oss;
+        for (size_t i = 2; i < split_msg.size(); ++i) {
+            if (i > 2) oss << ' ';
+            oss << split_msg[i];
+        }
+        j["args"] = {
+            {"user", split_msg[1]},
+            {"text", oss.str()}
+        };
+    }
+    else if (cmd == "history") {
+        j["args"] = {
+            {"room", split_msg[1]},
+            {"limit", split_msg[2]}
+        };
+    }
+    else if (cmd == "history_dm") {
+        j["args"] = {
+            {"user", split_msg[1]},
+            {"limit", split_msg[2]}
+        };
+    }
+    string dump = j.dump(2);
+    ofstream out(format("{}json.txt", req_id));
+    out << dump;
+    return j;
+}
 
 int main() {
     int sock = 0;
@@ -18,7 +93,7 @@ int main() {
 
     // create socket file descriptor
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        std::cerr << "Socket creation error" << std::endl;
+        cerr << "Socket creation error" << endl;
         return -1;
     }
     serv_addr.sin_family = AF_INET;
@@ -26,19 +101,24 @@ int main() {
 
     // convert ipv4 and ipv6 addresses from text to binary
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        std::cerr << "Invalid address/ Address not supported" << std::endl;
+        cerr << "Invalid address/ Address not supported" << endl;
         return -1;
     }
 
     // connect to server
     if (connect(sock, reinterpret_cast<struct sockaddr *>(&serv_addr), sizeof(serv_addr)) < 0) {
-        std::cerr << "Connection Failed" << std::endl;
+        cerr << "Connection Failed" << endl;
         return -1;
     }
+
+    int req_id = 1;
+
     for (;;) {
-        std::string msg;
-        std::cout << "Enter message: ";
-        std::getline(std::cin, msg);
+        string msg;
+        cout << "Enter message: ";
+        getline(cin, msg);
+        json req = msg_to_json(req_id, msg);
+        req_id++;
         send(sock, msg.c_str(), msg.size(), 0);
         if (msg == "exit") {
             break;
